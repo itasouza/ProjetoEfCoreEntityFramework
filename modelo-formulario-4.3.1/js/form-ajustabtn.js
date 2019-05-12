c32864d69af180d1a7b81523ee305b9b84aea819
itasouza <itasouza@yahoo.com.br>
2019-05-12 02:34:56 -0300

    Ajuste de Formulário

--- modelo-formulario-4.3.1/js/form-ajustabtn.js
+++ modelo-formulario-4.3.1/js/form-ajustabtn.js
@@ -10,12 +10,29 @@ function addClasses() {
     });
 }
 
+function addClassesListagem(){
+      $(".bg-light").each(function () {
+        var scre = $("body").width();
+        if (scre >= 768) {
+            $("#listagem").addClass("p-5");
+            $("#listagem").removeClass("p-3");
+        } else {
+            $("#listagem").removeClass("p-5");
+            $("#listagem").addClass("p-3");
+        }
+
+    });  
+}
+
+
 $(document).ready(function () {
     // Adicionar classes ao carregar o documento
     addClasses();
+    addClassesListagem();
 
     $(window).resize(function () {
         // Adicionar sempre que a tela for redimensionada
         addClasses();
+        addClassesListagem();
     });
 });
